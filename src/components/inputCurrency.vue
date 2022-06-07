0fcccb63d6a23be2f88904f6bf24e27cce7a446d
Danila <da_buddha@bk.ru>
2022-06-07 18:22:52 +0300

    50% логики инпутов готово

--- src/components/inputCurrency.vue
+++ src/components/inputCurrency.vue
@@ -1,23 +1,20 @@
 <template>
-<div class="relative cursor-pointer w-full">
-  <input @focus="isActive = true" @blur="isActive = false" :class="{'rounded-b-md': !isActive}" class="rounded-t-md l-4 focus:placeholder:opacity-0 placeholder:transition-opacity bg-slate-100 border  border-gray-300 placeholder:text-gray-400 focus:border-gray-300 min-w-full  focus:ring-0 placeholder:pr-10" placeholder="Search"  type="text">
-  <div class="absolute top-2 right-0 flex items-center flrx-row pointer-events-none">
+<div class="relative cursor-pointer w-full modalFix">
+  <input @focus="setActive" v-model="sortCurr" :placeholder="(typeof propValue == 'number')?propValue:'Search'" :class="{'rounded-b-md': !isActive}" class="rounded-t-md l-4 focus:placeholder:opacity-0 placeholder:transition-opacity bg-slate-100 border  border-gray-300 placeholder:text-gray-400 focus:border-gray-300 min-w-full  focus:ring-0 placeholder:pr-10"   type="text">
+  <div class="absolute top-2 right-0 flex items-center flex-row pointer-events-none">
     <img class="border-l pl-10 border-gray-200 w-auto mr-4" :class="{'opacity-0': isActive}" :src="choosedCurr.image" alt="">
-    <span class=" mt-1 mr-8  w-7" :class="{'opacity-0': isActive}" >{{choosedCurr.ticker?choosedCurr.ticker.toUpperCase():'' || '&nbsp;'}}</span>
+    <span class="mt-1 mr-8  w-10 overflow-hidden" :class="{'opacity-0': isActive}" >{{choosedCurr.ticker?choosedCurr.ticker.toUpperCase():'' || '&nbsp;'}}</span>
     <ArrowIcon :class="{'rotate-180': isActive}" class="mr-3 transition-all"/>
   </div>
-  <ul class="absolute w-full rounded-b-md bg-slate-100 transition-opacity z-40" :class="{'opacity-0 pointer-events-none': !isActive}">
-  
+  <ul class="absolute w-full rounded-b-md bg-slate-100 transition-opacity z-40 max-h-36 overflow-y-scroll"  :class="{'opacity-0 hidden':!isActive}">
     <li @click="chooseCurr(item)" class="flex flex-row items-center pl-4 pt-3 pb-3 hover:bg-slate-200" v-for="item in filterCur" :key="item.name">
-      <img class="mr-3" :src="item.image" :alt="item.ticker">
+      <img class="mr-3 w-4" :src="item.image" :alt="item.ticker">
       <span class="mr-4">{{item.ticker.toUpperCase()}}</span>
       <span>{{item.name}}</span>
     </li>
-
   </ul>
 </div>
 </template>
-
 <script>
 import ArrowIcon from '@/components/icons/ArrowIcon.vue'
 import {mapGetters} from 'vuex'
@@ -25,21 +22,41 @@ export default {
     data() {
         return {
            isActive: false,
-           choosedCurr: ''
+           sortCurr: '',
         }
+    },
+    props: {
+      choosedCurr: Object,
+      propValue: Number 
     },
     components: {
         ArrowIcon
     },
     methods: {
       chooseCurr(e){
-          this.choosedCurr = e
+        this.$emit('changeCurr',e)
+        this.isActive = false
+        this.sortCurr = ''
+        window.removeEventListener('click',this.clickHandler)
+
+      },
+      clickHandler(e) {
+        if (!this.$el.contains(e.target)) {
+          this.isActive=false
+          window.removeEventListener('click',this.clickHandler)
+          this.sortCurr = ''
+        }
+      },
+      setActive() {
+        window.addEventListener('click',this.clickHandler)
+        this.isActive = true
       }
     },
+
     computed: {
         ...mapGetters(['curr']),
         filterCur() {
-          return this.curr.slice(0, 3)
+          return (!this.sortCurr?this.curr:this.curr.filter((el)=>el.name.toLowerCase().includes(this.sortCurr.toLowerCase())))
         }
     }
     
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="ex">
+  <section class="ex select-none">
     <h1 class="ex__title">
       Crypto Exchange
     </h1>
@@ -7,18 +7,54 @@
       Exchange fast and easy
     </span>
     <div class="ex__input-wrap flex items-center flex-col md:flex-row">
-      <inputCurrency class="ex__select"/>
-      <SwapIcon class="ex-img cursor-pointer md:mx-7 ml-auto md:my-0 my-3 md:rotate-0 rotate-90"/>
-      <inputCurrency class="ex__select"/>
+      <inputCurrency :propValue="minAm"  :choosedCurr="choosedCurr1" @changeCurr="changeCurr('choosedCurr1',$event)" class="ex__select"/>
+      <SwapIcon @click="swap" class="ex-img cursor-pointer md:mx-7 ml-auto md:my-0 my-3 md:rotate-0 rotate-90"/>
+      <inputCurrency :propValue="est" :choosedCurr="choosedCurr2" @changeCurr="changeCurr('choosedCurr2',$event)" class="ex__select"/>
     </div>
     
   </section>
 </template>
 
 <script>
+import axios from 'axios'
 import inputCurrency from "@/components/inputCurrency.vue";
 import SwapIcon from "@/components/icons/SwapIcon.vue";
 export default {
+  data() {
+    return {
+       choosedCurr1: {},
+       choosedCurr2: {},
+       minAm: null,
+       est: null,
+       apikey: 'c9155859d90d239f909d2906233816b26cd8cf5ede44702d422667672b58b0cd',
+    }
+  },
+  methods: {
+    swap(){
+      [this.choosedCurr1,this.choosedCurr2]=[this.choosedCurr2,this.choosedCurr1]
+      this.fetchMinAmount()
+      this.fetchEst()
+    },
+    async changeCurr(type,value) {
+      this[type] = value
+      if (Object.keys(this.choosedCurr1).length && Object.keys(this.choosedCurr2).length) {
+        await this.fetchMinAmount()
+        this.fetchEst()
+      }
+
+    },
+    async fetchMinAmount() {
+       this.minAm = ((await axios.get(`https://api.changenow.io/v1/min-amount/${this.choosedCurr1.ticker}_${this.choosedCurr2.ticker}?api-key=${this.apikey}`))
+       .data)
+       .minAmount
+    },
+    async fetchEst() {
+        this.est = ((await axios.get(`https://api.changenow.io/v1/exchange-amount/${this.minAm}/${this.choosedCurr1.ticker}_${this.choosedCurr2.ticker}?api_key=${this.apikey}`))
+        .data)
+        .estimatedAmount
+    }
+  },
+  
   components: {
     inputCurrency,SwapIcon
   },
